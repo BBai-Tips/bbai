5760923ffaa26d472a371d0ad65579be9001da5f
Charlie Garrison <charlie-github@garrison.com.au>
2024-09-03 21:44:52 +1000

    reformat

--- bui/src/routes/_middleware.ts
+++ bui/src/routes/_middleware.ts
@@ -1,20 +1,20 @@
-import { FreshContext } from "$fresh/server.ts";
+import { FreshContext } from '$fresh/server.ts';
 
 export async function handler(req: Request, ctx: FreshContext) {
-  const origin = req.headers.get("Origin") || "*";
-  const resp = await ctx.next();
-  const headers = resp.headers;
+	const origin = req.headers.get('Origin') || '*';
+	const resp = await ctx.next();
+	const headers = resp.headers;
 
-  headers.set("Access-Control-Allow-Origin", origin);
-  headers.set("Access-Control-Allow-Credentials", "true");
-  headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
-  );
-  headers.set(
-    "Access-Control-Allow-Methods",
-    "POST, OPTIONS, GET, PUT, DELETE",
-  );
+	headers.set('Access-Control-Allow-Origin', origin);
+	headers.set('Access-Control-Allow-Credentials', 'true');
+	headers.set(
+		'Access-Control-Allow-Headers',
+		'Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With',
+	);
+	headers.set(
+		'Access-Control-Allow-Methods',
+		'POST, OPTIONS, GET, PUT, DELETE',
+	);
 
-  return resp;
+	return resp;
 }
--- bui/src/tailwind.config.ts
+++ bui/src/tailwind.config.ts
@@ -1,11 +1,11 @@
-import { type Config } from "npm:tailwindcss@3.3.5";
+import { type Config } from 'npm:tailwindcss@3.3.5';
 
 export default {
-  content: [
-    "{routes,islands,components}/**/*.{ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		'{routes,islands,components}/**/*.{ts,tsx}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 } satisfies Config;
