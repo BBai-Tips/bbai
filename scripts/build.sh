f34f57403aa43b9997b81f9635e77be67d6536e3
Charlie Garrison <charlie-github@garrison.com.au>
2024-09-27 20:37:03 +1000

    bat the stop

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,6 @@ DEST_DIR="${1:-/usr/local/bin}"
 deno task build
 
 # Copy build files to the specified destination directory
-cp build/* "$DEST_DIR"
+cp build/bbai build/bbai-api "$DEST_DIR"
 
 echo "Installation complete. Files copied to $DEST_DIR"
